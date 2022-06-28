3657ea7a33cf02d55102394ea2851f46c58c89aa
Gabriela Arrupes <103344390+gabrielarrupes@users.noreply.github.com>
2022-06-28 00:31:25 -0300

    manipulação de arrays através do método forEach

---
+++ mediaforEach.js
@@ -0,0 +1,15 @@
+// para cada elemento do array execute o que está dentro do bloco de código;
+// forEach percorre a array sem necessidade de criar o for;
+
+const notas = [10, 9, 7.5];
+
+let somaDasNotas = 0;
+
+
+notas.forEach( nota => { 
+    somaDasNotas += nota
+})
+
+let media = somaDasNotas/notas.length
+
+console.log(media);
