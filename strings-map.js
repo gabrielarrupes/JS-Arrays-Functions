5aeea26c85e2dec287c483a454df4b36ac6a0483
Gabriela Arrupes <103344390+gabrielarrupes@users.noreply.github.com>
2022-07-05 21:36:28 -0300

    manipulando strings com o método map

---
+++ strings-map.js
@@ -0,0 +1,7 @@
+//padronizando strings com o método map;
+
+let nomes = ["giordan silva", "gustavo braga", "GASPAR Oliveira"]
+
+const nomesPadronizados = nomes.map(nome => nome.toUpperCase());
+
+console.log(nomesPadronizados);
