c8a4c67d883c94307244c2393bb8f61a1bfca98a
Gabriela Arrupes <103344390+gabrielarrupes@users.noreply.github.com>
2022-07-21 23:43:01 -0300

    concatenando arrays com concat

---
+++ concat.js
@@ -0,0 +1,7 @@
+
+const salaDeJava = [" Bella", " Bruna", " Bianca"];
+const salaDePHP = [ " Charlotte", " Carol", " Charlie"];
+
+const salasUnificadas = salaDeJava.concat(salaDePHP);
+
+console.log(`Juntando as duas turmas temos a seguinte lista de alunas: ${salasUnificadas}.`);
